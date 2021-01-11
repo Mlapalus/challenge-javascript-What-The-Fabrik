2d5e1b5bcec2f5d05686e053d606264e528b9558
mlapalus <mlapalus@yahoo.fr>
2021-01-11 21:41:57 +0100

    localstorage with JSON

--- scripts/Repository/CardRepository.js
+++ scripts/Repository/CardRepository.js
@@ -9,29 +9,12 @@ class CardRepository
       localStorage.setItem('nbCard', `${nbCard}`);
     }
 
-    const prefix = localStorage.getItem('nbCard');
-    localStorage.setItem(prefix+"-title",card.title);
-    localStorage.setItem(prefix+"-image_url",card.image_url);
-    localStorage.setItem(prefix+"-score",card.score);
-    localStorage.setItem(prefix+"-episodes",card.episodes);
-    localStorage.setItem(prefix+"-id",card.mal_id);
+    const prefix = localStorage.getItem('nbCard');    
+    localStorage.setItem(prefix + "card",JSON.stringify(card));
   }
 
   static find(i){
-    let data = {
-      "title": "",
-      "image_url": "",
-      "score": "",
-      "episodes": "",
-      "mal_id": "",
-    };
-    data = {
-            "title": localStorage.getItem(i+"-title"),
-            "image_url": localStorage.getItem(i+"-image_url"),
-            "score": localStorage.getItem(i+"-score"),
-            "episodes": localStorage.getItem(i+"-episodes"),
-            "mal_id": localStorage.getItem(i+"-id"),
-    }
-    return data;   
+    const card = localStorage.getItem(i + "card");
+    return card && JSON.parse(card);
   }
 }
